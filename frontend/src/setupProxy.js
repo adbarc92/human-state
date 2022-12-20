85bdb1d34502893e3ba6556af998b0dd02272a52
adbarc92 <adbarclay92@gmail.com>
2022-12-20 15:46:52 -0700

    Hmm

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -6,7 +6,7 @@ const context = [
 
 module.exports = function (app) {
     const appProxy = createProxyMiddleware(context, {
-        target: 'https://localhost:5058',
+        target: 'https://localhost:7272',
         secure: false
     });
 
